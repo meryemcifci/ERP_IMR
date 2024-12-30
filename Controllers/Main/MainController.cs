25e3885d65d884fd0103fbca8fb5472308574984
Meryem ÇİFCİ <meryem.cifci@ogr.sakarya.edu.tr>
2024-12-30 18:22:37 +0300

    main conroller eklendi.

---
+++ Controllers/Main/MainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERP_IMR.Controllers.Main
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
